223e3573cfeb0bd98ed4b5f12a8d755fd9dc5fdc
omardevelops <omarahmadp91@gmail.com>
2022-08-10 02:38:25 +0400

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
