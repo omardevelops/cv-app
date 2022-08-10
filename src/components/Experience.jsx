d32c2f550ddfc35289be6984d38dc8f767e7e45a
omardevelops <omarahmadp91@gmail.com>
2022-08-10 18:44:16 +0400

    add: education, experience, general components

---
+++ src/components/Experience.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Experience extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div className="Experience">
+        <h1>Hello world!</h1>
+      </div>
+    );
+  }
+}
+
+export default Experience;
---
+++ src/components/General.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class General extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div className="General">
+        <h1>Hello world!</h1>
+      </div>
+    );
+  }
+}
+
+export default General;
