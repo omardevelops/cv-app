40265d525685593171f98ae3c621fa59c17891b4
omardevelops <omarahmadp91@gmail.com>
2022-08-22 18:03:34 +0400

    add: testInfo object

---
+++ src/testInfo.js
@@ -0,0 +1,36 @@
+const testInfo = {
+  general: [
+    {
+      firstName: 'Omar',
+      lastName: 'Ahmad',
+      email: 'omarahmadp91@gmail.com',
+      phone: '0528222985',
+      summary:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse suscipit risus id suscipit faucibus. Nam volutpat et risus ac rhoncus. Fusce eu tincidunt purus. Aliquam dictum tincidunt mauris, ut dapibus lectus pretium vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis metus odio. Ut semper dolor vel ante fringilla, at dictum nibh vestibulum. Sed ut sodales nisi. Maecenas tempor augue tempus lacinia blandit. Donec odio arcu, dapibus nec leo vel, vehicula pulvinar urna. Proin ullamcorper vitae nulla et pharetra. Mauris et viverra ex. Nullam sit amet aliquet arcu. Nullam libero ex, tincidunt vel pharetra id, semper et nisi.',
+      id: 'yrRodt98w-HWsIsRNxa10',
+    },
+  ],
+  education: [
+    {
+      id: 'f8Dlm6XwGhuH8OZsxGdXE',
+      institutionName: 'Khalifa University',
+      titleOfStudy: 'Computer Engineering',
+      GPA: '3.87',
+      startDate: '2017-08-27',
+      endDate: '2022-06-13',
+    },
+  ],
+  experience: [
+    {
+      id: 'jafFSEa7xZAXYx9IT9byC',
+      companyName: 'DP World',
+      positionTitle: 'Engineer Intern',
+      mainTasks:
+        "Engineering based internship at System & Automation department in Terminal 1. Was trained on DP\nWorld's complex crane systems. My tasks included: Check and rectify daily RTG and ARMG crane data\nin an SQL database, build and setup computer camera network hardware (NVR), and develop an Arduino\nproject that allows translation of analog signals from/to digital signals.",
+      startDate: '2021-05-27',
+      endDate: '2021-07-27',
+    },
+  ],
+};
+
+export default testInfo;
